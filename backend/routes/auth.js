7534a5bf98cacbe96fab519f6067c5b7c47d0b2f
Hrishi1810 <hvjoshi2001@gmail.com>
2023-04-26 21:32:17 +0530

    2nd commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { query, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
+const User = require('../models/User');
 
-router.post('/' ,query('person').notEmpty(), (req, res)=>{
+router.post('/' ,[
+  body('name').isLength({min : 3}),
+  body('email').isEmail(),
+  body('password').isLength({min : 5}),
+], (req, res)=>{
     const result = validationResult(req);
   if (result.isEmpty()) {
-    return res.send(`Hello, ${req.query.person}!`);
+    return res.status(400).json({errors : errors.array()});
   }
-
-  res.send({ errors: result.array() });
+  User.create({
+    name : req.body.name,
+    password : req.body.password,
+    email : req.body.email,
+  }).then(user=>res.json(user));
+  res.send(req.body);
 })
 
 module.exports = router;
